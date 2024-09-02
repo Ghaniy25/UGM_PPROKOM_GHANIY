4f62741c530f282fbc3f6478d2c6da8e10e3b5e8
Ghaniy Zulianto <ghaniyzulianto@mail.ugm.ac.id>
2024-09-02 12:34:10 +0700

    Add files via upload

---
+++ week 3/Tugas 5 week 3.py
@@ -0,0 +1,20 @@
+blog = 'Klinik' + 'Python'
+blog
+print("hasil penggabungan blog:",blog )
+newblog = blog*5
+newblog
+print("hasil pengulangan newblog:",newblog )
+blog *= 4
+print(blog)
+string = 'Klinik' 'Python'
+string
+print("hasil gabungan string:",string )
+buah = 'Nanas'
+buah[0]
+buah[0:2]
+buah[0:4]
+buah[0:5]
+print("karakter awal:",buah[0])
+print("karakter awal sampai 2:",buah[0:2])
+print("karakter awal sampai 4:",buah[0:4])
+print("karakter awal sampai 5:",buah[0:5])
